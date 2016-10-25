fdf04bafed9c457670218c665eac00ad09a3311b
bvanseghi <bosvan16@gmail.com>
2016-10-25 15:30:25 -0400

    change to Magma Block

--- src/main/java/net/bvanseghi/starcraft/tileentity/ModTileEntities.java
+++ src/main/java/net/bvanseghi/starcraft/tileentity/ModTileEntities.java
@@ -11,6 +11,8 @@ public class ModTileEntities {
 		GameRegistry.registerTileEntity(TileEntityBlockRichVespeneGeyser.class, "RichVespeneGeyser");
 		GameRegistry.registerTileEntity(TileEntityBlockVespeneGeyserChar.class, "VespeneGeyserChar");
 		GameRegistry.registerTileEntity(TileEntityBlockRichVespeneGeyserChar.class, "RichVespeneGeyserChar");
+		GameRegistry.registerTileEntity(TileEntityBlockVespeneGeyserChar.class, "VespeneGeyserShakuras");
+		GameRegistry.registerTileEntity(TileEntityBlockRichVespeneGeyserChar.class, "RichVespeneGeyserShakuras");
 		GameRegistry.registerTileEntity(TileEntityBlockWarpGateWormholeChar.class, "WarpGateWormholeChar");
 		GameRegistry.registerTileEntity(TileEntityBlockWarpGateWormholeShakuras.class, "WarpGateWormholeShakuras");
 		GameRegistry.registerTileEntity(TileEntityBlockWarpGateWormholeOverworld.class, "WarpGateWormholeOverworld");
