4580c65e47eab385da6c13927357fe9db72ed350
wundrweapon <whhacker.dcx@gmail.com>
2017-01-06 16:43:19 -0500

    Minor changes

--- src/main/java/net/bvanseghi/starcraft/blocks/BlockTeleporter.java
+++ src/main/java/net/bvanseghi/starcraft/blocks/BlockTeleporter.java
@@ -4,8 +4,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockPortal;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -16,22 +14,17 @@ public class BlockTeleporter extends BlockPortal {
 	}
 
 	@Override
-	 public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
-    {
-        if (!entityIn.isRiding() && !entityIn.isBeingRidden() && entityIn.isNonBoss())
-        {
+	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        if(!entityIn.isRiding() && !entityIn.isBeingRidden() && entityIn.isNonBoss()) {
             entityIn.setPortal(pos);
         }
     }
 
-	public boolean trySpawnPortal(World worldIn, BlockPos pos)
-    {
+	@Override
+	public boolean trySpawnPortal(World worldIn, BlockPos pos) {
         return false;
     }
 
 	@Override
-	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn)
-    {
-       
-    }
+	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn) {}
 }
--- src/main/java/net/bvanseghi/starcraft/blocks/BlockZergCreep.java
+++ src/main/java/net/bvanseghi/starcraft/blocks/BlockZergCreep.java
@@ -3,66 +3,63 @@ package net.bvanseghi.starcraft.blocks;
 import java.util.Random;
 
 import net.bvanseghi.starcraft.items.ModItems;
-import net.bvanseghi.starcraft.lib.StarcraftConfig;
+import net.bvanseghi.starcraft.lib.Reference;
 import net.minecraft.block.BlockDirt;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
 public class BlockZergCreep extends ModBlocks {
-
-	public static final String name = "zergCreep";
-
 	public BlockZergCreep() {
 		super(Material.GROUND);
+		setRegistryName(new ResourceLocation(Reference.RL_BASE + "zerg_creep"));
+		setUnlocalizedName(Reference.UN_BASE + "zergCreep");
 		setSoundType(SoundType.SAND);
 		setHardness(5.0F);
 		setResistance(10.0F);
 		setHarvestLevel("shovel", 2);
-		this.setTickRandomly(true);
+		setTickRandomly(true);
 	}
 
-	public Item getItemDropped(int par1, Random par2, int par3) {
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
 		return ModItems.creepResin;
 	}
 
-	public int quantityDropped(Random par1) {
-		return 2 + par1.nextInt(2);
+	@Override
+	public int quantityDropped(Random rand) {
+		return 2 + rand.nextInt(2);
 	}
 
+	@Override
 	protected boolean canSilkHarvest() {
 		return true;
 	}
 
 	// TODO: Redo the Zerg Creep effect
-	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		if (!worldIn.isRemote) {
-			if (worldIn.getLightFromNeighbors(pos.up()) < 4
-					&& worldIn.getBlockState(pos.up()).getLightOpacity(worldIn, pos.up()) > 2) {
+	@Override
+	public void randomTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+		if(!worldIn.isRemote) {
+			if(worldIn.getLightFromNeighbors(pos.up()) < 4 && worldIn.getBlockState(pos.up()).getLightOpacity(worldIn, pos.up()) > 2) {
 				worldIn.setBlockState(pos, Blocks.DIRT.getDefaultState());
-			} else {
-				if (worldIn.getLightFromNeighbors(pos.up()) >= 9) {
-					for (int i = 0; i < 4; ++i) {
-						BlockPos blockpos = pos.add(rand.nextInt(3) - 1, rand.nextInt(5) - 3, rand.nextInt(3) - 1);
+			} else if(worldIn.getLightFromNeighbors(pos.up()) >= 9) {
+				for(int i = 0; i < 4; i++) {
+					BlockPos blockpos = pos.add(rand.nextInt(3) - 1, rand.nextInt(5) - 3, rand.nextInt(3) - 1);
 
-						if (blockpos.getY() >= 0 && blockpos.getY() < 256 && !worldIn.isBlockLoaded(blockpos)) {
-							return;
-						}
+					if(blockpos.getY() >= 0 && blockpos.getY() < 256 && !worldIn.isBlockLoaded(blockpos)) {
+						return;
+					}
 
-						IBlockState iblockstate = worldIn.getBlockState(blockpos.up());
-						IBlockState iblockstate1 = worldIn.getBlockState(blockpos);
+					IBlockState currState= worldIn.getBlockState(blockpos);
+					IBlockState upState = worldIn.getBlockState(blockpos.up());
 
-						if (iblockstate1.getBlock() == Blocks.DIRT
-								&& iblockstate1.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT
-								&& worldIn.getLightFromNeighbors(blockpos.up()) >= 4
-								&& iblockstate.getLightOpacity(worldIn, pos.up()) <= 2) {
-							worldIn.setBlockState(blockpos, Blocks.GRASS.getDefaultState());
-						}
+					if (currState.getBlock() == Blocks.DIRT && currState.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT && worldIn.getLightFromNeighbors(blockpos.up()) >= 4 && upState.getLightOpacity(worldIn, pos.up()) <= 2) {
+						worldIn.setBlockState(blockpos, Blocks.GRASS.getDefaultState());
 					}
 				}
 			}
