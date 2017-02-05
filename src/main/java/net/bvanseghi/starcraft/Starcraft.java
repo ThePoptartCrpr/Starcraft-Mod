04aa8724133439dc92b6c6a946b300294c557f06
wundrweapon <whhacker.dcx@gmail.com>
2017-02-05 11:37:35 -0500

    Tried to fix things

--- src/main/java/net/bvanseghi/starcraft/Starcraft.java
+++ src/main/java/net/bvanseghi/starcraft/Starcraft.java
@@ -1,13 +1,7 @@
 package net.bvanseghi.starcraft;
 
-import net.bvanseghi.starcraft.items.ItemC14GaussRifle;
 import net.bvanseghi.starcraft.lib.Reference;
 import net.bvanseghi.starcraft.proxy.ServerProxy;
-import net.bvanseghi.starcraft.renderer.RenderItemC14GaussRifle;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -17,20 +11,16 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = "[1.10], [1.10.2]")
+@Mod(modid = Reference.MODID, acceptedMinecraftVersions = "[1.10.2]", useMetadata = true)
 public class Starcraft {
 	static {
 		FluidRegistry.enableUniversalBucket();
 	}
 
 	//Testing the C14 Gauss Rifle
-	public static ItemC14GaussRifle itemTest = (ItemC14GaussRifle) new ItemC14GaussRifle().setFull3D()
-			.setUnlocalizedName("c14gaussrifle").setCreativeTab(CreativeTabs.TOOLS).setRegistryName("c14gaussrifle");
+//	public static ItemC14GaussRifle itemTest = (ItemC14GaussRifle) new ItemC14GaussRifle().setFull3D()
+//			.setUnlocalizedName("c14gaussrifle").setCreativeTab(CreativeTabs.TOOLS).setRegistryName("c14gaussrifle");
 
 	@Instance(Reference.MODID)
 	public static Starcraft instance;
@@ -50,8 +40,8 @@ public class Starcraft {
 		proxy.registerModelBakeryStuff();
 		
 		MinecraftForge.EVENT_BUS.register(this);
-		GameRegistry.register(itemTest);
-	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemTest, 0, new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"));
+//		GameRegistry.register(itemTest);
+//	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemTest, 0, new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"));
 	}
 
 	@EventHandler
@@ -60,9 +50,9 @@ public class Starcraft {
 	}
 
 	//Probably more C14 Gauss Rifle testing
-	@SideOnly(Side.CLIENT)
-	@SubscribeEvent
-	public void onModelBake(ModelBakeEvent event) {
-	    event.getModelRegistry().putObject(new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"), new RenderItemC14GaussRifle());
-	}
+//	@SideOnly(Side.CLIENT)
+//	@SubscribeEvent
+//	public void onModelBake(ModelBakeEvent event) {
+//	    event.getModelRegistry().putObject(new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"), new RenderItemC14GaussRifle());
+//	}
 }
--- src/main/java/net/bvanseghi/starcraft/fluids/ModFluids.java
+++ src/main/java/net/bvanseghi/starcraft/fluids/ModFluids.java
@@ -1,10 +1,11 @@
 package net.bvanseghi.starcraft.fluids;
 
+import net.bvanseghi.starcraft.blocks.ModBlocks;
 import net.bvanseghi.starcraft.lib.Reference;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
 public class ModFluids {
-	public static Fluid acid = new Fluid("acid", new ResourceLocation(Reference.RL_BASE + "acid_fluid_still"), new ResourceLocation(Reference.RL_BASE + "acid_fluid_still")); //FIXME: get a flowing texture and set it here
-	public static Fluid blood = new Fluid("blood", new ResourceLocation(Reference.RL_BASE + "blood_fluid_still"), new ResourceLocation(Reference.RL_BASE + "blood_fluid_flowing"));
+	public static Fluid acid = new Fluid("acid", new ResourceLocation(Reference.RL_BASE + "acid_fluid_still"), new ResourceLocation(Reference.RL_BASE + "acid_fluid_still")).setBlock(ModBlocks.FLUID_ACID); //FIXME: get a flowing texture and set it here
+	public static Fluid blood = new Fluid("blood", new ResourceLocation(Reference.RL_BASE + "blood_fluid_still"), new ResourceLocation(Reference.RL_BASE + "blood_fluid_flowing")).setBlock(ModBlocks.FLUID_BLOOD);
 }
