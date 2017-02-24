722512333a78c9f406c7075aed12459bcca4a61c
wundrweapon <whhacker.dcx@gmail.com>
2017-02-24 00:00:42 -0500

    What was changed? Good question...

--- src/main/java/net/bvanseghi/starcraft/Starcraft.java
+++ src/main/java/net/bvanseghi/starcraft/Starcraft.java
@@ -1,7 +1,13 @@
 package net.bvanseghi.starcraft;
 
+import net.bvanseghi.starcraft.items.ItemC14GaussRifle;
 import net.bvanseghi.starcraft.lib.Reference;
 import net.bvanseghi.starcraft.proxy.ServerProxy;
+import net.bvanseghi.starcraft.renderer.RenderItemC14GaussRifle;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -11,6 +17,10 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = Reference.MODID, acceptedMinecraftVersions = "[1.10.2]", useMetadata = true)
 public class Starcraft {
@@ -19,8 +29,8 @@ public class Starcraft {
 	}
 
 	//Testing the C14 Gauss Rifle
-//	public static ItemC14GaussRifle itemTest = (ItemC14GaussRifle) new ItemC14GaussRifle().setFull3D()
-//			.setUnlocalizedName("c14gaussrifle").setCreativeTab(CreativeTabs.TOOLS).setRegistryName("c14gaussrifle");
+	public static ItemC14GaussRifle itemTest = (ItemC14GaussRifle) new ItemC14GaussRifle().setFull3D()
+			.setUnlocalizedName("c14gaussrifle").setCreativeTab(CreativeTabs.TOOLS).setRegistryName("c14gaussrifle");
 
 	@Instance(Reference.MODID)
 	public static Starcraft instance;
@@ -40,8 +50,8 @@ public class Starcraft {
 		proxy.registerModelBakeryStuff();
 		
 		MinecraftForge.EVENT_BUS.register(this);
-//		GameRegistry.register(itemTest);
-//	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemTest, 0, new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"));
+		GameRegistry.register(itemTest);
+	    Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemTest, 0, new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"));
 	}
 
 	@EventHandler
@@ -50,9 +60,9 @@ public class Starcraft {
 	}
 
 	//Probably more C14 Gauss Rifle testing
-//	@SideOnly(Side.CLIENT)
-//	@SubscribeEvent
-//	public void onModelBake(ModelBakeEvent event) {
-//	    event.getModelRegistry().putObject(new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"), new RenderItemC14GaussRifle());
-//	}
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onModelBake(ModelBakeEvent event) {
+	    event.getModelRegistry().putObject(new ModelResourceLocation("starcraft:c14gaussrifle", "inventory"), new RenderItemC14GaussRifle());
+	}
 }
