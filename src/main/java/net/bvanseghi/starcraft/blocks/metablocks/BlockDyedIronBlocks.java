8026a8fea0e0ab3e0b8ed4809ebfb8bf1bacd04b
bvanseghi <bosvan16@gmail.com>
2017-02-25 10:37:16 -0500

    more sound/material fixes

--- src/main/java/net/bvanseghi/starcraft/blocks/metablocks/BlockDyedIronBlocks.java
+++ src/main/java/net/bvanseghi/starcraft/blocks/metablocks/BlockDyedIronBlocks.java
@@ -5,6 +5,7 @@ import java.util.List;
 import net.bvanseghi.starcraft.blocks.metablocks.EnumHandler.DyedIronType;
 import net.bvanseghi.starcraft.lib.Reference;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
@@ -34,7 +35,8 @@ public class BlockDyedIronBlocks extends Block implements IMetaBlockName {
 	 * @param registryName The block's registry name - defaultly the unlocalized name
 	 */
 	public BlockDyedIronBlocks(String unlocalizedName, String registryName) {
-		super(Material.ROCK);
+		super(Material.IRON);
+		this.setSoundType(SoundType.METAL);
 		this.setUnlocalizedName(unlocalizedName);
 		this.setRegistryName(new ResourceLocation(Reference.MODID, registryName));
 		this.setHardness(20); //Sets how hard the block is to break
