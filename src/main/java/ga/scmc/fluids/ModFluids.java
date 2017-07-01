e959a96e3c1ea22d843578bf39bc1aeb1ed063c9
wundrweapon <whhacker.dcx@gmail.com>
2017-07-01 13:45:06 -0400

    I'm not quite sure what my goal was here...

--- src/main/java/ga/scmc/fluids/ModFluids.java
+++ src/main/java/ga/scmc/fluids/ModFluids.java
@@ -4,17 +4,8 @@ import ga.scmc.blocks.ModBlocks;
 import ga.scmc.lib.Reference;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidRegistry;
 
 public class ModFluids {
-
-	public static Fluid acid = new Fluid("fluid_acid", new ResourceLocation(Reference.RL_BASE + "blocks/acid_fluid_still"), new ResourceLocation(Reference.RL_BASE + "blocks/acid_fluid_still")).setBlock(ModBlocks.FLUID_ACID); //FIXME: get a flowing texture and set it here
-	public static Fluid blood = new Fluid("fluid_blood", new ResourceLocation(Reference.RL_BASE + "blocks/blood_fluid_still"), new ResourceLocation(Reference.RL_BASE + "blocks/blood_fluid_flowing")).setBlock(ModBlocks.FLUID_BLOOD);
-
-	public static void register() {
-		acid.setUnlocalizedName(Reference.ModBlocks.FLUID_ACID.getUnlocalizedName());
-		blood.setUnlocalizedName(Reference.ModBlocks.FLUID_BLOOD.getUnlocalizedName());
-		FluidRegistry.registerFluid(acid);
-		FluidRegistry.registerFluid(blood);
-	}
+	public static Fluid acid = new Fluid("fluid_acid", new ResourceLocation(Reference.RL_BASE + "blocks/acid_fluid_still"), new ResourceLocation(Reference.RL_BASE + "blocks/acid_fluid_still")); //FIXME: get a flowing texture and set it here
+	public static Fluid blood = new Fluid("fluid_blood", new ResourceLocation(Reference.RL_BASE + "blocks/blood_fluid_still"), new ResourceLocation(Reference.RL_BASE + "blocks/blood_fluid_flowing"));
 }
